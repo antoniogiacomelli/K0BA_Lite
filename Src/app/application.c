93f9c51581d67658283608d228de0347d1e35e98
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2024-12-19 23:31:22 -0300

    V0.3.0

--- Src/app/application.c
+++ Src/app/application.c
@@ -19,72 +19,103 @@ UINT32 stack3[STACKSIZE];
  * Declare/init app objects
  * Declare  kernel objects: mutexes, semaphores, timers, etc.
  *******************************************************************************/
+#define FLAG_TEMP_SENSOR_UPDATE       (1U << 0)
+#define FLAG_HUM_SENSOR_UPDATE    (1U << 1)
+#define FLAG_CO2_SENSOR_UPDATE   (1U << 2)
+#define FLAG_FLOW_SENSOR_UPDATE   (1U << 3)
 
-#define QUEUE_SIZE (64)
+typedef enum
+{
+	TEMPERATURE = 1, HUMIDITY, CO2, FLOW
+} UPDATE_t;
 
-static UINT32 queue[QUEUE_SIZE];
-static UINT32 head=0;
-static UINT32 tail=0;
-
-K_MBOX mailbox; 
+K_MBOX mbox;
 
 /*** Init kernel objects here */
 VOID kApplicationInit(VOID)
 {
-    kMboxInit( &mailbox, 0, NULL);
-}
-
-static VOID send(UINT32* mesg)
-{
-	queue[head]=*mesg;
-	kMboxPost(&mailbox, &queue[head]);
-	head+=1U;
-	head %= QUEUE_SIZE;
-}
-
-static VOID recv(UINT32** recvpptr)
-{
-	UINT32* tailptr = 	&queue[tail];
-	kMboxPend(&mailbox, (ADDR*)&tailptr);
-	*recvpptr = tailptr;
-	tail+=1U;
-	tail %= QUEUE_SIZE;
-
+	kMboxInit(&mbox, EMPTY, NULL);
 }
 
 VOID Task1(VOID)
 {
-	UINT32 sendmesg=0;
-    while (1)
-    {
-    	send(&sendmesg);
-    	sendmesg++;
-    	kSleep(1);
-    }
+	while (1)
+	{
+		UINT32 sendFlag = 0;
+		UPDATE_t updateType = 0;
+
+		while (1)
+		{
+			updateType = (updateType + 1);
+			if (updateType > 4)
+			{
+				updateType = 1;
+			}
+			switch (updateType)
+			{
+			case (TEMPERATURE):
+				sendFlag = FLAG_TEMP_SENSOR_UPDATE;
+				break;
+			case (HUMIDITY):
+				sendFlag = FLAG_HUM_SENSOR_UPDATE;
+				break;
+			case (CO2):
+				sendFlag = FLAG_CO2_SENSOR_UPDATE;
+				break;
+			case (FLOW):
+				sendFlag = FLAG_FLOW_SENSOR_UPDATE;
+				break;
+			default:
+				break;
+			}
+			K_ERR err = kMboxSend(&mbox, (ADDR) &sendFlag, K_WAIT_FOREVER);
+			assert(err==0);
+		}
+
+	}
 }
 
 VOID Task2(VOID)
 {
+	UINT32 *rcvdFlag = 0;
+	TID senderPid = 0;
+	UINT32 wantedFlags = FLAG_TEMP_SENSOR_UPDATE | FLAG_HUM_SENSOR_UPDATE |
+	FLAG_CO2_SENSOR_UPDATE |
+	FLAG_FLOW_SENSOR_UPDATE;
+	UINT32 rcvdFlags = 0;
+	while (1)
+	{
+		 K_ERR err = kMboxRecv(&mbox, (ADDR) &rcvdFlag, &senderPid,
+				 K_WAIT_FOREVER);
+		if (err == 0)
+		{
+			if (senderPid == 1)
+			{
+				rcvdFlags |= *rcvdFlag;
+				if (rcvdFlags == wantedFlags)
+				{
+					rcvdFlags = 0; /* clear rcvd flags */
+					kprintf("Task synchronized\n\r");
+					/* do work */
+				}
+				else
+				{
+					kprintf("Still missing a flag\n\r");
+					kBusyDelay(5);
+				}
+			}
 
-
-    while (1)
-    {
-
-        kSleep(1);
-
-    }
+		}
+	}
 }
 
 
 VOID Task3(VOID)
 {
-	UINT32* recvptr;
+	while (1)
+	{
+		kSleep(1);
 
-    while (1)
-    {
-    	recv(&recvptr);
-    	(VOID)recvptr;
-    	kSleep(1);
-    }
+	}
+
 }
-
